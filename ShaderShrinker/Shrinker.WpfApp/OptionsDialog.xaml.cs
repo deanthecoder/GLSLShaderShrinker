ec4068a19d429cdf3665574bf0cc182b1d9dd49d
Dean Edis <dean.edis@globalgraphics.com>
2021-05-08 15:50:59 +0100

    Refactoring: Move options dialog XAML into its own WPF control.

---
+++ ShaderShrinker/Shrinker.WpfApp/OptionsDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="OptionsDialog.cs">
+//      Copyright (c) 2021 Dean Edis. All rights reserved.
+//  </copyright>
+//  <summary>
+//  This example is provided on an "as is" basis and without warranty of any kind.
+//  We do not warrant or make any representations regarding the use or
+//  results of use of this example.
+//  </summary>
+// -----------------------------------------------------------------------
+
+namespace Shrinker.WpfApp
+{
+    public partial class OptionsDialog
+    {
+        public OptionsDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
