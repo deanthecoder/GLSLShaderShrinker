fab6ca4c6f124d2ec28de6010381a485de138e28
Dean Edis <dean.edis@gmail.com>
2023-05-06 20:52:59 +0100

    Feature: Transpiler - Multi-threaded image generation.

--- GLSLRenderer/Program.cs
+++ GLSLRenderer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using System.Text;
 
 // ReSharper disable InconsistentNaming
@@ -12,35 +13,37 @@ namespace GLSLRenderer;
 
 public static class Program
 {
-    public static void Main(string[] args)
-    {
-	    var iResolution = GLSLProgBase.vec2(160, 90) * 1.0f;
-        var glslProg = new GLSLProg(iResolution);
-        
-        var mainImageMethod = typeof(GLSLProg).GetMethod("mainImage", BindingFlags.NonPublic | BindingFlags.Instance);
-        
-        Console.WriteLine($"Generating image {iResolution}...");
-        var pixels = Enumerable
-            .Range(0, (int)glslProg.iResolution.y)
-            .Reverse()
-            .SelectMany(
-                        y =>
-                        {
-                            return Enumerable
-                                .Range(0, (int)glslProg.iResolution.x)
-                                .Select(
-                                        x =>
-                                        {
-	                                        object[] parameters = { null!, new vec2(x, y) };
-	                                        mainImageMethod!.Invoke(glslProg, parameters);
-	                                        return (vec4)parameters[0];
-                                        });
-                        })
-            .ToArray();
-        Console.WriteLine("Writing image...");
-        WritePPM("/Users/Dean/Desktop/output.ppm", pixels, (int)glslProg.iResolution.x, (int)glslProg.iResolution.y);
-        Console.WriteLine("Done.");
-    }
+	public static void Main(string[] args)
+	{
+		var iResolution = GLSLProgBase.vec2(160, 90) * 1.0f;
+		var mainImageMethod = typeof(GLSLProg).GetMethod("mainImage", BindingFlags.NonPublic | BindingFlags.Instance);
+
+		Console.WriteLine($"Generating image {iResolution}...");
+		var totalPixels = (int)iResolution.x * (int)iResolution.y;
+		vec4[] pixels = new vec4[totalPixels];
+
+		// Measure the start time.
+		var stopwatch = new Stopwatch();
+		stopwatch.Start();
+
+		var glslProg = new GLSLProg(iResolution);
+		Parallel.For(0, totalPixels, index =>
+		{
+			var x = index % (int)iResolution.x;
+			var y = (int)iResolution.y - index / (int)iResolution.x - 1;
+			object[] parameters = { null!, new vec2(x, y) };
+			mainImageMethod!.Invoke(glslProg, parameters);
+			pixels[index] = (vec4)parameters[0];
+		});
+
+		// Calculate the elapsed time.
+		stopwatch.Stop();
+		Console.WriteLine($"Image generation took {stopwatch.Elapsed.TotalSeconds:F1}s");
+
+		Console.WriteLine("Writing image...");
+		WritePPM("/Users/Dean/Desktop/output.ppm", pixels, (int)glslProg.iResolution.x, (int)glslProg.iResolution.y);
+		Console.WriteLine("Done.");
+	}
     
     private static void WritePPM(string filePath, vec4[] pixels, int width, int height)
     {
